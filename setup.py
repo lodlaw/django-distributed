5fac480b2b83f91ba9730e3f7c634f957b0b6a01
Anh Pham <anhphamduy@outlook.com>
2022-11-23 01:00:53 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+from pathlib import Path
+
+# read the contents README.md
+dir = Path(__file__).parent
+long_description = (dir / "README.md").read_text()
 
 setup(name='django_distributed',
-      version='0.1',
-      description='A python for distributed databases setup in Django',
+      version='0.2',
+      description='Distributed databases setup in Django',
       url='https://github.com/anhphamduy/django-distributed',
       author='Anh Pham',
       author_email='anh.pham@lodlaw.com',
@@ -12,4 +17,7 @@ setup(name='django_distributed',
           'django-crum',
           'django',
       ],
-      zip_safe=False)
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+     )
